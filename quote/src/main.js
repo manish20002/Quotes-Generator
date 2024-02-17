06478e80a69f9fc2d4acbcb2db1a5986cf0f6213
manish20002 <manishthirani202@gmail.com>
2024-02-17 18:47:20 +0530

    this is main file

---
+++ quote/src/main.js
@@ -0,0 +1,16 @@
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/components/topt.css';
+import '../styles/components/main.css';
+import '../styles/components/quote.css';
+import '../styles/components/about.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+
+darkMode();
+mobileNav();
